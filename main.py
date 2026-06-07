30c61b7bae2dd4d0f6814497b2a043b053f6f4e6
sbag@fedora <101804633+Shnku@users.noreply.github.com>
2026-06-07 18:33:53 +0530

    Added Threading for process speaking background  - no interupt main GUI loop  Changes to be committed: 	modified:   main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import webbrowser
 from datetime import datetime
+from threading import Thread
 from urllib.parse import quote_plus
 
-from features.tell_joke import tell_joke
+from features.get_joke import get_joke
 from features.process_apps import open_app
 from features.process_folder import open_folder
 from features.process_website import open_website
@@ -31,60 +32,60 @@ def processCommand(command):
 
         if song:
             response_text = f"Playing {song}"
-            speak(response_text)
+            Thread(target=speak, args=[response_text], daemon=True).start()
             webbrowser.open(
                 f"https://www.youtube.com/results?search_query={quote_plus(song)}"
             )
         else:
             response_text = "Say song name"
-            speak(response_text)
+            Thread(target=speak, args=[response_text], daemon=True).start()
 
     elif command.startswith("search youtube"):
         query = command.replace("search youtube", "", 1).strip()
 
         if query:
             response_text = f"Searching YouTube for {query}"
-            speak(response_text)
+            Thread(target=speak, args=[response_text], daemon=True).start()
             webbrowser.open(
                 f"https://www.youtube.com/results?search_query={quote_plus(query)}"
             )
         else:
             response_text = "Say what you want to search on YouTube"
-            speak(response_text)
+            Thread(target=speak, args=[response_text], daemon=True).start()
 
     elif "news" in command:
         response_text = "Opening today's news"
-        speak(response_text)
+        Thread(target=speak, args=[response_text], daemon=True).start()
         webbrowser.open("https://news.google.com/topstories?hl=en-IN&gl=IN&ceid=IN:en")
 
     elif command in ["time", "what is the time", "current time"]:
         current_time = datetime.now().strftime("%I:%M %p")
         response_text = f"The time is {current_time}"
-        speak(response_text)
+        Thread(target=speak, args=[response_text], daemon=True).start()
 
     elif command in ["date", "today date", "what is the date"]:
         current_date = datetime.now().strftime("%d %B %Y")
         response_text = f"Today's date is {current_date}"
-        speak(response_text)
+        Thread(target=speak, args=[response_text], daemon=True).start()
 
     elif "joke" in command:
-        response_text = tell_joke()
-        speak(response_text)
+        response_text = get_joke()
+        Thread(target=speak, args=[response_text], daemon=True).start()
 
     elif command.startswith("tell me about"):
         topic = command.replace("tell me about", "", 1).strip()
 
         if topic:
             response_text = f"Searching about {topic}"
-            speak(response_text)
+            Thread(target=speak, args=[response_text], daemon=True).start()
             webbrowser.open(f"https://www.google.com/search?q={quote_plus(topic)}")
         else:
             response_text = "Say the topic name"
-            speak(response_text)
+            Thread(target=speak, args=[response_text], daemon=True).start()
 
     else:
         response_text = "Searching Google"
-        speak(response_text)
+        Thread(target=speak, args=[response_text], daemon=True).start()
         query = quote_plus(command)
         webbrowser.open(f"https://www.google.com/search?q={query}")
 
