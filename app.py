f51ad6cc0c3c63ef5197c900e6e58b7573f3c70b
sbag@fedora <101804633+Shnku@users.noreply.github.com>
2026-06-12 15:07:43 +0530

    Sample command Chips added into UI + some refinements 	features added for chip:   app.py

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
 import asyncio
-from concurrent.futures import ThreadPoolExecutor
 import random
 
 import flet as ft
 
 from main import processCommand
-
-# from speech import speak
 
 
 def main(page: ft.Page):
@@ -15,6 +12,7 @@ def main(page: ft.Page):
     page.window.always_on_top = True
     page.window.alignment = ft.Alignment.BOTTOM_LEFT
     page.window.brightness = ft.Brightness.DARK
+    page.padding = 0
 
     # --- THE TYPEWRITER EFFECT FUNCTION ---
     async def typewriter_effect(ui_text_control, full_text, delay=0.06):
@@ -22,38 +20,26 @@ def main(page: ft.Page):
         current_text = ""
         for letter in full_text:
             current_text += letter
-            print("log:::", letter)
+            # print("log:::", letter)
             ui_text_control.value = current_text
-            page.update()  # Use the async update method
+            page.update()
             delay = random.random() * 0.09
-            print("time---", delay)
+            # print("time---", delay)
             await asyncio.sleep(delay)  # Yields control so Flet can render
-
-    # def send_click(e):
-    #     chat.controls.append(ft.Text(new_message.value))
-    #     data = new_message.value
-    #     new_message.value = ""
-    #     speak(data)
-    #     response_data = processCommand(data)
-    #     chat.controls.append(
-    #         ft.Card(
-    #             ft.Text(response_data or "no response"),
-    #         )
-    #     )
-    #     page.update()
 
     async def send_click(e):
         user_message = new_message.value.strip()
         if not user_message:
             return
 
-        # 1. Instantly display user message
+        # Instantly display user message
         chat.controls.append(
             ft.Text(
                 f"You: {user_message}",
                 color=ft.Colors.BLUE_400,
                 align=ft.Alignment.CENTER_RIGHT,
                 text_align=ft.TextAlign.RIGHT,
+                margin=ft.Margin(right=12),
             )
         )
         new_message.value = ""
@@ -80,6 +66,7 @@ def main(page: ft.Page):
         expand=True,
         spacing=10,
         auto_scroll=True,
+        padding=8,
         adaptive=True,
     )
 
@@ -94,18 +81,98 @@ def main(page: ft.Page):
         on_submit=send_click,
     )
 
+    def toogle_chip_click(e):
+        for chip in chip_list:
+            if chip.selected:
+                # print("chip clicked", chip.label.value)
+                new_message.value = chip.label.value
+                chip.selected = False
+        page.update()
+
+    chip_list: list[ft.Chip] = [
+        ft.Chip(
+            label=ft.Text(tag, color=ft.Colors.BLUE_GREY_300, size=13),
+            shape=ft.RoundedRectangleBorder(radius=15),
+            padding=0,
+            selected=False,
+            show_checkmark=False,
+            on_select=toogle_chip_click,
+        )
+        for tag in [
+            "tell me about flet in python",
+            "What is the time",
+            "tell me a joke",
+            "play some music",
+            "open desktop",
+            "open downloads folder",
+            "Date",
+            "today's news",
+            "open pictures",
+            "Open google",
+            "Open notepad",
+        ]
+    ]
+
+    def toogle_chip_expand():
+        if chips_row.wrap is False:
+            chips_row.wrap = True
+            chip_toogle_icon.content = ft.Icon(ft.Icons.EXPAND_MORE_OUTLINED)
+        else:
+            chip_toogle_icon.content = ft.Icon(ft.Icons.EXPAND_LESS_OUTLINED)
+            chips_row.wrap = False
+
+    chip_toogle_icon = ft.Container(
+        content=ft.Icon(ft.Icons.EXPAND_LESS_OUTLINED),
+        on_click=toogle_chip_expand,
+    )
+
     page.add(
         chat,
-        ft.Row(
-            controls=[
-                new_message,
-                ft.IconButton(
-                    icon=ft.Icons.SEND,
-                    adaptive=True,
-                    icon_size=30,
-                    on_click=send_click,
-                ),
-            ]
+        ft.Container(
+            padding=12,
+            border_radius=20,
+            bgcolor=ft.Colors.BLACK_26,
+            expand_loose=True,
+            content=ft.Column(
+                alignment=ft.MainAxisAlignment.CENTER,
+                expand=True,
+                spacing=4,
+                controls=[
+                    ft.Row(
+                        spacing=2,
+                        controls=[
+                            chip_toogle_icon,
+                            ft.Text(
+                                "Sample Commands ..",
+                                size=11,
+                                color=ft.Colors.BLUE_GREY_200,
+                            ),
+                        ],
+                    ),
+                    ft.Container(
+                        chips_row := ft.Row(
+                            controls=chip_list,
+                            wrap=False,
+                            tight=True,
+                            spacing=5,
+                            run_spacing=7,
+                        ),
+                        clip_behavior=ft.ClipBehavior.ANTI_ALIAS,
+                    ),
+                    ft.Row(
+                        margin=ft.Margin(top=8),
+                        controls=[
+                            new_message,
+                            ft.IconButton(
+                                icon=ft.Icons.SEND,
+                                adaptive=True,
+                                icon_size=30,
+                                on_click=send_click,
+                            ),
+                        ],
+                    ),
+                ],
+            ),
         ),
     )
 
